7041aa1f637afde395c793184b1622b4e94e7f63
Ben Greanya <bengreanya@gmail.com>
2022-11-14 13:04:02 -0800

    added relative path in render utils and event listener in detail js

--- detail/detail.js
+++ detail/detail.js
@@ -4,6 +4,12 @@ import { renderDogDetail } from '../render-utils.js';
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
 // on load
+// eslint-disable-next-line space-before-function-paren
+window.addEventListener('load', async () => {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+});
 // get the id from URL
+
 // use the id to fetch the dog
 // render and append this dog's details to the container
