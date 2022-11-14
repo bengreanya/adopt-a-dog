97a6eadfda2854a914c3c7440ed46f5c720369a1
Ben Greanya <bengreanya@gmail.com>
2022-11-14 14:22:18 -0800

    fixed lint error

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
+// eslint-disable-next-line space-before-function-paren
 window.addEventListener('load', async () => {
     const dogs = await getDogs();
     for (let dog of dogs) {
